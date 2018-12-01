bd086b2a0ea69b6bc020b058dbff9fa433010d3b
Andrew <andrescastillo.r@hotmail.com>
2018-11-30 22:15:02 -0400

    ultrasound to be tested

--- ultrasound.asm
+++ ultrasound.asm
@@ -1,45 +1,59 @@
 list P = 16F877A
 include <P16F877A.inc>
 
-W_AUX EQU 20h
-
 org 0h
-		goto CommonConfig
-;********************************** Interrupcion ****************************************************************		
-org 4h
-		movwf W_AUX ; guardo el valor previo de W para no generar comportamientos raros
-		call RB0Int
-		movf W_AUX, 0 ; devuelvo el valor de W
-		retfie	
-;****************************************************************************************************************
-org 16h
-CommonConfig
 		bsf STATUS, 5
 		bcf TRISB, 1 ; RB1 es el Trig del sensor de ultrasonido
-		;************* Configuracion TMR1 ******************************
-		; configuro aqui el prescaler de T1CON (bits 5-4 => 11 = 1:8, 10 = 1:4, 01 = 1:2, 00 = 1:1)
-		bsf T1CON, 5 ; prescaler 1:8
-		bsf T1CON, 4
-		bsf T1CON, 3 ; habilito el oscilador
-		clrf TMR1H
-		clrf TMR1L
+		bcf TRISB, 2 ; un led cualquiera que me indica si estoy en proximidad
+		;************* Configuracion TMR0 ******************************
+		bcf OPTION_REG, 2 ; prescaler 16 para tener mejor precision de distancias cortas
 		;***************************************************************
-		
-		;************* Configuracion INT **********
-		bsf INTCON, 7 ; global
-		bsf INTCON, 6 ; int perifericos
-		bsf PIE1, 0 ; habilito la interrupcion del overflow del TMR1
-		bsf INTCON, 4 ; RB0 external interrupt
-		bcf OPTION_REG, 6 ; RB0 interrumpe en flanco de bajada
-		;******************************************
-        
 		bcf STATUS, 5
+		bcf PORTB, 2
 		bcf PORTB, 1
 		
-Main	nop
-		goto Main		
+Main	bsf PORTB, 1
+		call delay10us
+		bcf PORTB, 1
+		call startCounting ; el tmr0 va a empezar a contar desde 0
+NotYet	btfsc PORTB, 0 ; espero a que el pin de eco se baje, que es cuando recibe la respuesta
+		goto NotYet
+		call TMR0_OFF
+		movlw d'34' ; distancia cm = (X/Prescaler)*(1/2*29,1) => 10 cm = (X/16)*(1/2*29,1) => X = 33
+		subwf TMR0, 0
+		btfsc STATUS, 0 ; si TMR0 = 33 -> 33 - 34 = -1 -> C = 0, si esta en proximidad
+		goto NotClose
+		bsf PORTB, 2 ; se prende el led que me dice que estoy en proximidad
+		goto Main
+		
+NotClose
+		bcf PORTB, 2
+		goto Main
 	
-RB0Int
-		bcf INTCON, 1
-		; resto de la funcion
+delay10us
+		nop ; aqui hay 6 nops, el call y return cuentan como 4 micro segundos
+		nop
+		nop
+		nop
+		nop
+		nop
 		return
+
+TMR0_ON
+		bsf STATUS, 5
+		bcf OPTION_REG, 3 ; en esencia apago el TMR0
+		bcf STATUS, 5
+		return
+		
+TMR0_OFF
+		bsf STATUS, 5
+		bsf OPTION_REG, 3 ; en esencia prendo el TMR0
+		bcf STATUS, 5
+		return
+		
+startCounting
+		clrf TMR0
+		call TMR0_ON
+		return
+		
+end
